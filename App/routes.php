ae4f01e3b00d9014d17cb864d6b8eef7b6ca6ed8
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-18 14:36:43 -0300

    feat: introdução rotas

--- App/routes.php
+++ App/routes.php
@@ -1,13 +1,40 @@
 <?php
 
-// Includes Controllers
-
-
+use App\Controller\
+{
+    ChavePixController,
+    ContaController,
+    CorrentistaController,
+    TransacaoController
+};
 
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($url)
 {
+    /*case '/correntista/save':
+        CorrentistaController::save();
+    break;
+
+    case '/correntista/entrar':
+        CorrentistaController::entrar();
+    break;
+
+    case '/conta/pix/enviar':
+        ChavePixController::enviar();  ou ContaController 
+    break;
+
+    case '/conta/pix/receber':
+        ChavePixController::receber();  ou ContaController 
+    break;
     
+    case '/conta/extrato':
+        ContaController::extrato();
+    break; */
+
+    /* Rota não identificada */
+    default:
+        http_response_code(403);
+    break;
 }
