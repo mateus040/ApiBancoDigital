81ec85a81168ed9be257d4566d8803386fe07029
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-04-25 15:48:25 -0300

    feat: iniciando funções (Conta, Transação) Controller

---
+++ App/Controller/ContaController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+class ContaController extends Controller
+{
+    public static function save()
+    {
+
+    }
+
+    public static function select()
+    {
+
+    }
+
+    public static function extrato()
+    {
+        
+    }
+
+    public static function EnviarPix()
+    {
+
+    }
+
+    public static function ReceberPix()
+    {
+
+    }
+
+    public static function delete()
+    {
+
+    }
+}
---
+++ App/Controller/TransacaoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+class TransacaoController extends Controller
+{
+    public static function save()
+    {
+
+    }
+
+    public static function select()
+    {
+
+    }
+
+    public static function update()
+    {
+
+    }
+
+    public static function delete()
+    {
+        
+    }
+}
