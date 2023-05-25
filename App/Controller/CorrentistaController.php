ae9b829fe076c85ca8f7ccca939cd5eb4b282125
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-05-25 15:58:11 -0300

    corrigindo controller de correntista

--- App/Controller/CorrentistaController.php
+++ App/Controller/CorrentistaController.php
@@ -7,33 +7,48 @@ use Exception;
 
 class CorrentistaController extends Controller
 {
-    public static function entrar()
+    public static function login()
     {
-        //
+        try
+        {
+            $data = json_encode(file_get_contents('php://input'));
+
+            $model = new CorrentistaModel();
+
+            parent::getResponseAsJSON($model->getByCfpAndSenha($data->Cpf, $data->Senha));
+        }
+        catch(Exception $e)
+        {
+            parent::LogError($e);
+            parent::getExceptionAsJSON($e);
+        }
     }
 
     public static function save() : void
     {
         try
         {
-            $json_obj = json_decode(file_get_contents('php://input'));
+            $data = json_encode(file_get_contents('php://input'));
 
             $model = new CorrentistaModel();
-            $model->id = $json_obj->Id;
-            $model->nome = $json_obj->Nome;
-            $model->cpf = $json_obj->Cpf;
-            $model->data_nasc = $json_obj->DataNasc;
-            $model->senha = $json_obj->Senha;
 
-            $model->save();
+            foreach (get_object_vars($data) as $key => $value)
+            {
+                $prop_letra_minuscula = strtolower($key);
+
+                $model->$prop_letra_minuscula = $value;
+
+                parent::getExceptionAsJSON($model->save());
+            }
         }
-        catch (Exception $e)
+        catch(Exception $e)
         {
+            parent::LogError($e);
             parent::getExceptionAsJSON($e);
         }
     }
 
-    public static function listar() : void
+    /*public static function listar() : void
     {
         
     }
@@ -41,5 +56,5 @@ class CorrentistaController extends Controller
     public static function delete() : void
     {
 
-    }
+    }*/
 }
