5feee3f9456fb30ab89352a8560feffebdd63f3a
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-01 14:53:11 -0300

    corrigindo rotas

--- App/routes.php
+++ App/routes.php
@@ -2,7 +2,6 @@
 
 use App\Controller\
 {
-    ChavePixController,
     ContaController,
     CorrentistaController,
     TransacaoController
@@ -13,28 +12,49 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($url)
 {
-    case '/correntista/save':
+    // Backup
+    case '/exportar':
+        $return_var = NULL;
+        $output = NULL;
+        $command = 'C:/"Program Files"/MySQL/"MySQL Server 8.0"/bin/mysqldump -uroot -petecjau -P3307 -hlocalhost db_bancodigital > C:/Dev/file.sql';
+        exec($command, $output, $exit_code);
+        
+        echo "Backup realizado com sucesso.";
+    break;
+
+    // Correntista
+    case '/correntista/salvar':
         CorrentistaController::save();
+    break;
+
+    case '/correntista/entrar':
+        CorrentistaController::login();
+    break;
+
+    // Conta
+    case '/conta/abrir':
+        ContaController::abrir();
+    break;
+
+    case '/conta/fechar':
+        ContaController::fechar();
     break;
 
     case '/conta/extrato':
         ContaController::extrato();
     break;
 
-    case '/conta/pix/enviar':
-        ContaController::EnviarPix();
+    // Transação
+    case '/transacao/pix/receber':
+        TransacaoController::receberPix();
     break;
 
-    case '/conta/pix/receber':
-        ContaController::ReceberPix();
+    case '/transacao/pix/enviar':
+        TransacaoController::enviarPix();
     break;
 
-    case '/correntista/entrar':
-        CorrentistaController::entrar();
-    break;
 
-    /* Rota não identificada */
     default:
-        http_response_code(403);
+        header('HTTP/1.0 403 Forbidden');
     break;
 }
